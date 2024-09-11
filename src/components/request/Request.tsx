78c17ffd81c0134dc9fda2521fa806dfd01d2d46
dom-m17 <dominicmaynard00@gmail.com>
2024-09-11 13:48:36 +0300

    basic elements

---
+++ src/components/request/Request.tsx
@@ -0,0 +1,11 @@
+import SearchBox from "./search-box/SearchBox";
+import SearchButton from "./search-button/SearchButton";
+
+export default function Request() {
+    return (
+        <>
+            <SearchBox />
+            <SearchButton />
+        </>
+    )
+}
