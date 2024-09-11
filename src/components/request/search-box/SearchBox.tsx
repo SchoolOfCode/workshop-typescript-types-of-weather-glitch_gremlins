e609528861bfcac62fd8e875066cd498ed55e958
habib <habibali15@hotmail.com>
2024-09-11 13:34:37 +0100

    useState updates form value

--- src/components/request/search-box/SearchBox.tsx
+++ src/components/request/search-box/SearchBox.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBox() {
-
     const [inputValue, setInputValue] = useState("");
 
-    const handleInputChange = (event) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
-      };
+    };
 
     return (
-        <input type="search" value={inputValue} onChange={handleInputChange}/>
-    )
+        <input type="search" value={inputValue} onChange={handleInputChange} />
+    );
+    console.log(inputValue)
 }
