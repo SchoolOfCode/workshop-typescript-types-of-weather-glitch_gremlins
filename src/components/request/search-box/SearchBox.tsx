c65c5ab0d3ec3f66fcbd2aa985a49f30bf9a345a
habib <habibali15@hotmail.com>
2024-09-11 12:31:36 +0100

    updating state to reflect search value

--- src/components/request/search-box/SearchBox.tsx
+++ src/components/request/search-box/SearchBox.tsx
@@ -1,5 +1,14 @@
+import React, {useState} from 'react';
+
 export default function SearchBox() {
+
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+      };
+
     return (
-        <input type="search" />
+        <input type="search" value={inputValue} onChange={handleInputChange}/>
     )
 }
